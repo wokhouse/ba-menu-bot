ddeeb3a93d7d3b6b0e3fa6e1d4c2f0ffd74ce325
Jade <jade@fungj.net>
2019-12-11 23:35:25 -0800

    parse out menu items and form tweet text strings

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const fetchMenuData = async () => {
 const main = async () => {
   const data = await fetchMenuData();
   // menu is stored in parts of the day
-  const dayParts = data.days[0].cafes[224].dayparts[0];
+  const dayParts = data.days[0].cafes[cafeID].dayparts[0];
   // parse start and end times
   const parsedParts = dayParts.map((p, i) => {
     const start = moment(p.starttime, 'HH:mm');
@@ -31,7 +31,7 @@ const main = async () => {
   });
   // check to see if we are 1 hour away from a meal
   const closeToMeal = parsedParts.map((p) => {
-    const now = moment();
+    const now = moment('11:00', 'HH:mm');
     const { start } = p;
     // past 1 hour before meal starts
     const withinAnHour = start.subtract(1, 'hour').isBefore(now);
@@ -41,8 +41,69 @@ const main = async () => {
     return false;
   });
   const nextNearMealIndex = closeToMeal.indexOf(true);
-  const nextNearMeal = dayParts[2];
-  console.log(nextNearMeal);
+  // if there's no meal in < 1hr, don't do anything
+  if (nextNearMealIndex === -1) return false;
+  // index of which meal is in an hour w/in dayParts array
+  const nextNearMeal = dayParts[nextNearMealIndex];
+  // go through each station and get tier 1 items
+  const nextMealWithItems = nextNearMeal.stations.map((m) => {
+    const { items, label: stationLabel } = m;
+    // get item data using the keys included in the station items
+    const thisStationItems = items.map((i) => data.items[i]);
+    // filter out tier 1 items (the big ones, exclude things like sugar and pickles)
+    const tier1Items = thisStationItems.filter((i) => {
+      // if item does not contain a tier key, don't include in list
+      if ('tier' in i === false) return false;
+      // if item has 1 as tier, include in list
+      if (i.tier === 1) return true;
+      // if item has something other than 1 in tier, don't include in list
+      return false;
+    });
+    // just get item name, description and labels (eg vegan, veg)
+    const itemData = tier1Items.map(
+      ({
+        label,
+        description,
+        cor_icon, // eslint-disable-line camelcase
+      }) => {
+        // parse out veg, vegan, gluten free
+        const noticeNumbers = Object.keys(cor_icon).filter(
+          (i) => Array.from([1, 4, 9]).indexOf(parseInt(i, 10)) > -1,
+        );
+        // convert numbers to labels
+        const notices = noticeNumbers.map((n) => {
+          switch (n) {
+            case ('1'): { return 'vegetarian'; }
+            case ('4'): { return 'vegan'; }
+            case ('9'): { return 'gluten-free'; }
+            default: { return null; }
+          }
+        });
+        return ({
+          label,
+          description,
+          notices,
+        });
+      },
+    );
+    const stationWithItems = { label: stationLabel, items: itemData };
+    return stationWithItems;
+  });
+  // filter out stations that aren't serving any items (ie stations from other meals)
+  const nextMeal = nextMealWithItems.filter((s) => s.items.length > 0);
+  // get the name of the meal
+  const nextMealName = nextNearMeal.label;
+  // convert each station into a string
+  const stationStrings = nextMeal.map((s) => {
+    const stationName = s.label;
+    const items = s.items.map(({ label, notices }) => `${label} ${notices.length ? `(${notices.join(' ')})` : ''}`);
+    const stationString = `== ${stationName} ==\n${items.join('\n')}`;
+    return stationString;
+  });
+  // make array of tweet texts
+  const tweets = [`Reed College Commons ${nextMealName} ${moment().format('MMM Do YYYY')}`].concat(stationStrings);
+  tweets.map((t) => console.log(t, '\n'));
+  return true;
 };
 
 main();
